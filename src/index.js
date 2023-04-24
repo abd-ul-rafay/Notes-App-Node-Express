6c2706d9e79fd54febed99efb6af3772bce88e3d
Abdul Rafay <abdulrafaykhatana@gmail.com>
2023-04-24 19:10:20 +0500

    Updated Second Time

--- src/index.js
+++ src/index.js
@@ -12,7 +12,12 @@ app.use(bodyParser.json());
 const Note = require("./models/note");
 
 const mongodbPath = "mongodb+srv://abdulrafaykhatana:abdulrafay@cluster0.itquoe2.mongodb.net/notesdb";
-mongoose.connect(mongodbPath).then(()=> {
+mongoose.connect(mongodbPath, {
+    dbName: "notesdb",
+    ssl: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> {
 
     // app routes
     app.get("/", (req, res)=> {
@@ -21,11 +26,10 @@ mongoose.connect(mongodbPath).then(()=> {
 
     const noteRouter = require("./routes/note")
     app.use("/notes", noteRouter);
-
-});
-
-// start a server on a port
-const port = process.env.port || 5000;
-app.listen(port, ()=> {
-    console.log("Server started at port " + port);
+    
+    // start a server on a port
+    const port = process.env.port || 5000;
+    app.listen(port, ()=> {
+        console.log("Server started at port " + port);
+    });
 });
